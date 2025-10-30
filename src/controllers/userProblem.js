00e433e8fa3cdbe40055d054c06f0898b195b9a6
Shubham singh <shubham1041singh@gmail.com>
2025-10-30 18:19:22 +0530

    createdUserProblem.js

---
+++ src/controllers/userProblem.js
@@ -0,0 +1,25 @@
+
+
+const createProblem = async (req , res)=>{
+
+    const{title , description , difficulty , 
+        tags , visibleTestCases , hiddenTestCases , startCode , referenceSolution , problemCreator
+     } = req.body
+
+     try{
+
+    for(const {language , completeCode} of referenceSolution)
+        {
+
+        }
+
+
+     }
+     catch(err)
+     {
+
+     }
+
+
+
+}
--- src/models/problem.js
+++ src/models/problem.js
@@ -72,12 +72,28 @@ const problemSchema = new Schema({
      }
     ],
 
+    referenceSolution:[
+        {
+            language:{
+                type: String,
+                required:true,
+            },     
+            completeCode :{
+                type:String,
+                required:true
+            }
+        }
+    ],
+
+    
+
 
     problemCreator:{
         type: Schema.Types.ObjectId,
         ref:'user',
         required:true
     }
+
 
 })
 
