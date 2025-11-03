d3fe2f30782b34324f817b1dbea28ab27fd3df28
Shubham singh <shubham1041singh@gmail.com>
2025-11-03 12:38:48 +0530

    submissionUpdated

---
+++ src/models/submission.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const submissionSchema = new Schema(
+    {
+        userId :{
+            type : Schema.Types.ObjectId,
+            ref : 'User',
+            required : true
+        },
+
+        problemId : {
+            type : Schema.Types.ObjectId,
+            ref : 'Problem',
+            required : true
+        },
+
+        code:{
+            type : String ,
+            required : true,
+        },
+
+        language : {
+            type : String ,
+            required : true,
+            enum : ['javascript' , 'python' , 'cpp' , 'java' , 'c' , 'typescript']
+        },
+
+        status :{
+            type : String,
+            enum : ['pending' , 'accepted' , 'wrong' , 'error'],
+        },
+
+        runtime:{
+            type : Number ,
+            default : 0
+        },
+
+        memory:{
+              type : Number ,
+              default : 0
+        },
+
+        errorMessage:{
+            type : String ,
+            default : ''
+        },
+
+        testCasePassed :{
+            type : Number ,
+            default : 0
+        }
+    },
+    {
+       timestamps : true
+    }
+);
+
+const Submission = mongoose.model('submission' , submissionSchema);
+
+
